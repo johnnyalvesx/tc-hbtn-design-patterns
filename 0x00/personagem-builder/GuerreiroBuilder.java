811fbcb36c2c1c30b09bf16bc8f12083ac415744
johnnya1ves <johnnyalves@ciandt.com>
2022-05-04 17:41:01 -0300

    Builder de personagens

---
+++ 0x00/personagem-builder/GuerreiroBuilder.java
@@ -0,0 +1,45 @@
+public class GuerreiroBuilder implements Builder {
+
+    private String nome;
+    private TipoPersonagem tipo;
+    private int inteligencia, forca, vigor, resistencia, destreza;
+
+    public Guerreiro build() {
+        return new Guerreiro(nome, inteligencia, forca, vigor, resistencia, destreza);
+    }
+
+    @Override
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void setTipo(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    @Override
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    @Override
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    @Override
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    @Override
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+}
