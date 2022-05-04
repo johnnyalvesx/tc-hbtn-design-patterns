1e860196c0f6d4805c40f1a9929ec3b210b7a79c
johnnya1ves <johnnyalves@ciandt.com>
2022-05-04 17:09:44 -0300

    Correção no cálculo de dano do guerreiro

--- 0x00/personagem/Guerreiro.java
+++ 0x00/personagem/Guerreiro.java
@@ -2,6 +2,7 @@ public class Guerreiro extends Personagem {
 
     public Guerreiro(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
         super(nome, TipoPersonagem.GUERREIRO, inteligencia, forca, vigor, resistencia, destreza);
+
         if (forca < inteligencia || forca < destreza) {
             throw new RuntimeException("Atributos invalidos para GUERREIRO");
         }
@@ -9,6 +10,6 @@ public class Guerreiro extends Personagem {
 
     @Override
     public double getDanoAtaque() {
-        return (getForca() * 0.8) + (getVigor() * 0.05) + (getDestreza() + 0.1) + (getInteligencia() + 0.05);
+        return (getForca() * 0.8) + (getVigor() * 0.05) + (getDestreza() * 0.1) + (getInteligencia() * 0.05);
     }
 }
--- 0x00/personagem/Mago.java
+++ 0x00/personagem/Mago.java
@@ -2,6 +2,7 @@ public class Mago extends Personagem {
 
     public Mago(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
         super(nome, TipoPersonagem.MAGO, inteligencia, forca, vigor, resistencia, destreza);
+
         if (inteligencia < forca || inteligencia < destreza) {
             throw new RuntimeException("Atributos invalidos para MAGO");
         }
