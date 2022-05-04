ae5037fba5d976f2ddfaaeb21c226f6f46e51a18
johnnya1ves <johnnyalves@ciandt.com>
2022-05-04 17:13:33 -0300

    Adição de dois espaços no toString de Personagem

--- 0x00/personagem/Personagem.java
+++ 0x00/personagem/Personagem.java
@@ -23,14 +23,14 @@ public abstract class Personagem {
     @Override
     public String toString() {
         return "Personagem{" +
-                "nome='" + nome + '\'' +
+                " nome='" + nome + '\'' +
                 ", tipo=" + tipo +
                 ", inteligencia=" + inteligencia +
                 ", forca=" + forca +
                 ", vigor=" + vigor +
                 ", resistencia=" + resistencia +
                 ", destreza=" + destreza +
-                ", dano ataque=" + String.format("%.2f", getDanoAtaque()) +
+                ", dano ataque=" + String.format("%.2f ", getDanoAtaque()) +
                 '}';
     }
 
